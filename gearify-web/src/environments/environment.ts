a2667ef35cd0b736bc393b43e296c2a412e10399
Jameel <vm.jameel@gmail.com>
2025-11-04 22:55:03 -0500

    fix: auth service was not hitting break point

--- gearify-web/src/environments/environment.ts
+++ gearify-web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '',
+  apiUrl: 'http://localhost:8080',
   stripePublishableKey: 'pk_test_YOUR_KEY_HERE',
   paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_HERE',
   enableLogging: true,
